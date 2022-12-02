cc2952fd5c183ec0b92ef903fa1259f56129168c
wzs <wzs@qq.com>
2022-12-02 16:45:27 +0800

    second commit

---
+++ testGit/src/testGit.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+/**
+ * @ClassName testGit
+ * @Description
+ * @Author WangZhisheng
+ * @Date 16:43 2022/12/2
+ * @Version 11.0.15
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
