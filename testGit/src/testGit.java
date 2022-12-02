4eebbf079dda24374f5955235c70d2de9e95c3c4
wzs <wzs@qq.com>
2022-12-02 19:04:05 +0800

    hot-fix commit

--- testGit/src/testGit.java
+++ testGit/src/testGit.java
@@ -10,5 +10,6 @@ import java.sql.SQLOutput;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world update version");
     }
 }
